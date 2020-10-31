29f01e4800e60e18935ab3ea35640f617ce96c4b
tejashree <tejashreech.7777@gmail.com>
2020-11-01 02:23:27 +1100

    python Variables

---
+++ variables.py
@@ -0,0 +1,21 @@
+#Variables are containers for storing data values.
+
+#Unlike other programming languages, Python has no command for declaring a variable.
+
+#A variable is created the moment you first assign a value to it.
+# creating variables: variables are used to store values.
+# 1:
+x= 5
+y= "teju"
+
+# 2: data type of assigned variables can be changed in a timely manner whenever required
+x= 5
+x= "teju"
+
+#string variables can be declared/ assigned within "" / '' quotes 
+
+#Rules for Python variables:
+#A variable name must start with a letter or the underscore character
+#A variable name cannot start with a number
+#A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+#Variable names are case-sensitive (age, Age and AGE are three different variables)
