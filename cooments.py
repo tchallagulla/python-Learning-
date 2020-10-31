f12ad4acaf41847ed042c8339fa53a31f5d09505
tejashree <tejashreech.7777@gmail.com>
2020-11-01 01:45:46 +1100

    python comments

---
+++ cooments.py
@@ -0,0 +1,13 @@
+#In python comments is helpful for code readability and understanding the functionality of the code and the components.
+#it is also helpful when executing the code in the production environment and can exclude it from testing or include it.
+
+#coomenting can be done in two ways:
+#1.using "#" at the end of each line /
+print("Hello teju") #welcome message
+
+#2. multiline string (triple quotes)
+""" 
+This is a comment
+written in
+more than just one line
+"""
